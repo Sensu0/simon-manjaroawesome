72125b01c7a5108e00daedbd91c4fd2c972d888e
Simon Brännström <simon.brannstrom1@gmail.com>
2020-09-12 17:40:37 +0300

    2

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #install packages commonly used by myself
-sudo pacman -S vim chromium brave libreoffice virtualbox htop neofetch ristretto kdenlive manjaro-printer hplip steam obs-studio glances timeshift discord man tldr yay base-devel pcmanfm qterminal gimp qbittorrent vlc snes9x-gtk lutris ttf-dejavu-mono-powerline
+sudo pacman -S vim chromium brave libreoffice virtualbox htop neofetch ristretto kdenlive manjaro-printer hplip steam obs-studio glances timeshift discord man tldr yay base-devel pcmanfm qterminal gimp qbittorrent vlc snes9x-gtk lutris ttf-dejavu-mono-powerline noto-fonts-cjk
