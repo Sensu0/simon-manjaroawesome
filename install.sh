3f8efbc8d98196d439414fe14d6a1603beca81f9
Simon Brännström <simon.brannstrom1@gmail.com>
2020-12-24 02:57:13 +0200

    updated package lists

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 #
 #tools
 sudo pacman -Syu
-sudo pacman -S --noconfirm vim libreoffice virtualbox htop neofetch ristretto kdenlive manjaro-printer \
-hplip obs-studio glances timeshift man tldr yay base-devel pcmanfm qterminal gimp qbittorrent vlc \
-gnome-disk-utility gparted \
+sudo pacman -S --noconfirm vim libreoffice-still libreoffice-still-sv virtualbox htop neofetch ristretto \
+manjaro-printer okular hplip glances timeshift man tldr yay base-devel pcmanfm qterminal gimp \
+qbittorrent gnome-disk-utility gparted wget bash bashtop zsh links kdeconnect lshw ranger \
+`#multimedia` \
+kdenlive vlc obs-studio subtitleeditor \
 `#browsers` \
 chromium firefox \
 `#gaming` \
-steam snes9x-gtk vbam-wx lutris discord \
+steam snes9x-gtk vbam-wx lutris discord mednafen ppsspp \
 `#fonts` \
 noto-fonts-cjk noto-fonts
--- install2.sh
+++ install2.sh
@@ -3,11 +3,13 @@
 #This will install all packages from AUR I use
 #
 #other
-yay -S --noconfirm --sudoloop brave-bin unetbootin subtitleedit slack-desktop teamviewer \
+yay -S --noconfirm --sudoloop brave-bin unetbootin slack-desktop teamviewer \
+discord-rpc-api keyman-onboard \
 `#fonts` \
 ttf-win10 noto-fonts-all ttf-ms-fonts steam-fonts ttf-dejavu-sans-mono-powerline-git \
 `#emulators` \
-pcsx2-git dolphin-emu-beta-git pcsxr-pgxp-git kega-fusion rpcs3-git \
+pcsx2-git dolphin-emu-beta-git pcsxr-pgxp-git kega-fusion rpcs3-git mupen64plus-git m64py mednaffe \
+desmume-git \
 `#games` \
 gzdoom dhewm3 \
 `#entertainment` \
